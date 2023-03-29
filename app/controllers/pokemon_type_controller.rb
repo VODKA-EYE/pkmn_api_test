b9ccebe773ec93c723923d2d012e74c4d484121f
VODKA-EYE <igorspbrush@gmail.com>
2023-03-29 23:57:07 +0300

    empty commit

--- app/controllers/pokemon_type_controller.rb
+++ app/controllers/pokemon_type_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PokemonTypeController < BaseController
-  # GET /pokemon_types
+  # GET /pokemon_types 
   # displays all pokemon types
   def all
     pokemon_types = PokemonType.all.order(:id)
